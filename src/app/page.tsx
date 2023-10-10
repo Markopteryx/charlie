356ee5b19cdd594a5499fd15106da03006c78e5f
Marko <marko.uksanovic1@gmail.com>
2023-10-10 22:37:31 +1000

    changed some markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700">
-            Charlie grows in power
+            Charlie - the sophisticated butler
           </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Chat with your <span className="text-blue-600"> documents </span> in
-          seconds!
+          Engage effortlessly with your{' '}
+          <span className="text-blue-600">documentation</span> in mere moments!
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Charlie lets you talk to your PDF files. Just upload your files and
-          start talking.
+          Charlie empowers you to interact seamlessly with your PDF documents.
+          Simply upload your files and commence eloquent dialogue.
         </p>
 
         <Link
@@ -27,7 +27,8 @@ export default function Home() {
           href="/dashboard"
           target="_blank"
         >
-          Get started <ArrowRight className="ml-2 h-5 w-5" />
+          Initiate Your Journey
+          <ArrowRight className="ml-2 h-5 w-5" />
         </Link>
       </MaxWidthWrapper>
 
